d3c777d5c0fd0a5bf7242572683b317fe31c3877
WaysideTester <dylanmurray211@outlook.com>
2018-11-15 19:22:40 -0600

    logging user's google display name

--- src/javascripts/data/auth.js
+++ src/javascripts/data/auth.js
@@ -3,7 +3,13 @@ import 'firebase/auth';
 
 const googleAuth = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      console.log(result.user.displayName);
+    })
+    .catch((err) => {
+      console.error('you done goofed', err);
+    });
 };
 
 const logout = () => firebase.auth().signOut();
